b72d540590785dd2a2197cb387374d94ef180355
김정미 <mayblueths@businesson.co.kr>
2013-10-01 17:51:36 -0700

    Customer Support Page Dev Rev.1

---
+++ source/BIZ_PORTAL/src/main/java/com/portal/controller/help/InquiryController.java
@@ -0,0 +1,132 @@
+package com.portal.controller.help;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.portal.common.collection.SBox;
+import com.portal.common.parent.SuperController;
+import com.portal.service.help.InquiryService;
+/*
+import com.bizDebn.common.enumtype.EUserErrorCode;
+import com.bizDebn.common.session.SessionManager;
+*/
+
+/**
+ * <pre>
+ * 일대일 문의 Controller
+ * </pre>
+ * @author JUNG MI KIM
+ * @since 2013. 10. 1.
+ * @version 1.0
+ * @package com.portal.controller.help.InquiryController.java
+ */
+@Controller
+public class InquiryController extends SuperController{
+
+	@Autowired
+	private InquiryService inquiryService;
+	
+	/**
+	 * <pre>
+	 * 1:1 등록 문의 등록화면  - Form
+	 * </pre>
+	 * @author JUNG MI KIM
+	 * @since 2013. 10. 1.
+	 * @version 1.0
+	 * @param sBox
+	 * @return
+	 */
+	@RequestMapping(value = "/help/addInquiryForm.do")
+	public ModelAndView addInquiryForm(@ModelAttribute("initBoxs") SBox sBox) {
+		ModelAndView mav = new ModelAndView(); //.jsp로 열어질 파일
+		mav.setViewName("help/addInquiryForm");  
+		
+		return mav;
+	}
+	
+	
+	/**
+	 * <pre>
+	 * 1:1 등록 문의 등록화면  - 등록 DB
+	 * </pre>
+	 * @author JUNG MI KIM
+	 * @since 2013. 10. 1.
+	 * @version 1.0
+	 * @param sBox
+	 * @return
+	 */
+	@RequestMapping(value = "/help/addInquiry.do")
+	public ModelAndView addInquiry(@ModelAttribute("initBoxs") SBox sBox) {
+		
+		SBox resultBox = inquiryService.addInquiry(sBox);
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/help/getInquiryList"); //redirect:
+		return mav;
+	}
+	
+	
+	
+	/*참고자료 
+	 * 
+	 * method = RequestMethod.POST)
+	public ModelAndView login(HttpServletRequest request,
+			@RequestParam(value = "custType", required = true) String custType,
+			@RequestParam(value = "loginId", required = true) String loginId,
+			@RequestParam(value = "loginPw", required = true) String loginPw ){
+	 * 
+	 * 
+	 * @RequestMapping(value = "/help/addInquiry.do")
+	public ModelAndView addInquiry(@ModelAttribute("initBoxs") SBox sBox) {
+		
+		SBox resultBox = inquiryService.addInquiry(sBox);
+		ModelAndView mav = new ModelAndView("redirect:/help/getInquiryList.do");
+		mav.addObject("sBox", resultBox);
+
+		return mav;
+	}*/
+	
+	
+	
+	
+	
+	
+	/** 1:1문의 등록(실제 DB ADD/ 리스트 
+	 * <pre>
+	 * Inquiry Controller Method
+	 * </pre>
+	 * @author JUNG MI KIM
+	 * @since 2013. 9. 23.
+	 * @version 1.0
+	 * @param request
+	 * @param custType : 법인,개인
+	 * @return
+	 */	
+	/*@RequestMapping(value = "/help/getInquiryList.do", method = RequestMethod.POST)
+	public ModelAndView getInquiryList(HttpServletRequest request,
+			@RequestParam(value = "titleText", required = true) String titleText){	
+		
+		ModelAndView mav = new ModelAndView(); 
+		mav.setViewName("/help/getInquiryList");  
+		
+		//System.out.println("***********************************************"+titleText);
+		
+		mav.addObject("result", inquiryService.addInquiry(sBox);
+		//mav.addObject("sBox", sBox);
+		return mav;
+		
+
+		
+	}*/
+	
+		
+		
+}
---
+++ source/BIZ_PORTAL/src/main/java/com/portal/dao/help/InquiryDao.java
@@ -0,0 +1,79 @@
+package com.portal.dao.help;
+
+import org.springframework.stereotype.Repository;
+
+import com.portal.common.exception.BizException;
+import com.portal.common.collection.SBox;
+import com.portal.common.collection.SBoxList;
+import com.portal.common.parent.SuperDao;
+
+/**
+ * <pre>
+ *   Notice Dao Class
+ * </pre>
+ * @author JUNG MI KIM
+ * @since 2013. 9. 11.
+ * @version 1.0
+ * @package com.portal.dao.help.NoticeDao.java
+ */
+
+@Repository
+public class InquiryDao extends SuperDao {
+	
+	/**
+	 * <pre>
+	 * 일대일 등록화면 DB등록 
+	 * </pre>
+	 * @author JUNG MI KIM
+	 * @since 2013. 10. 1.
+	 * @version 1.0
+	 * @param sBox
+	 * @return
+	 * @throws BizException
+	 */
+	public SBox insertInquiry(SBox sBox)  throws BizException{ 
+
+		SBox returnBox = null;
+		try {
+			returnBox = new SBoxList<SBox>(super.getSqlMapClientTemplate().queryForList("help.insertInquiry_SQL", sBox)).get(0);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			throw new BizException("21", "insertInquiry_SQL Dao ERROR");
+		}
+		return returnBox;
+	}
+	
+	
+	public SBoxList<SBox> getInquiryList(SBox sBox)  throws BizException{ 
+
+		SBoxList<SBox> resultList = null;
+		
+		try {
+			resultList = new SBoxList<SBox>(super.getSqlMapClientTemplate().queryForList("help.selectInquiryList_SQL", sBox));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			throw new BizException("21", "selectInquiryList_SQL Dao ERROR");
+		}
+
+		return resultList;
+	}
+	
+	
+	
+	public int selectMainTotalCount(SBox sBox)  throws BizException{ 
+
+		int result = 0;
+		try {
+			result = ((SBox) super.getSqlMapClientTemplate().queryForObject("notice.selectMainTotalCount_SQL", sBox)).getInt("totalCnt");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			throw new BizException("21", "selectMainTotalCount_SQL Dao ERROR");
+		}
+
+		return result;
+	}
+	
+
+	
+
+}
